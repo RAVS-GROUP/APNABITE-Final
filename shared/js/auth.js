8784eb0068dbd1e0547deaf5abb625d877cc82c2
RAVS <shubham110267@gmail.com>
2026-09-17 12:49:51 +0530

    Create auth.js

---
+++ shared/js/auth.js
@@ -0,0 +1,18 @@
+const Auth = {
+
+  isLoggedIn() {
+    return SessionManager.isLoggedIn();
+  },
+
+  getUser() {
+    const session = SessionManager.get();
+
+    return session
+      ? session.user
+      : null;
+  },
+
+  logout() {
+    SessionManager.clear();
+  }
+};
