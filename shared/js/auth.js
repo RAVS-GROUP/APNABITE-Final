6ce0b4c354a96e22e44f0a9f0371792bdc297e6b
RAVS <shubham110267@gmail.com>
2026-09-17 14:39:36 +0530

    Update auth.js

--- shared/js/auth.js
+++ shared/js/auth.js
@@ -1,18 +1,1 @@
-const Auth = {
 
-  isLoggedIn() {
-    return SessionManager.isLoggedIn();
-  },
-
-  getUser() {
-    const session = SessionManager.get();
-
-    return session
-      ? session.user
-      : null;
-  },
-
-  logout() {
-    SessionManager.clear();
-  }
-};
