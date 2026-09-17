7509d5753a9f394582c84dea79c875dacc97448c
RAVS <shubham110267@gmail.com>
2026-09-17 14:47:22 +0530

    Update cart.js

--- shared/js/cart.js
+++ shared/js/cart.js
@@ -1,19 +1,1 @@
-const CartManager = {
 
-  KEY: "apnabite_cart",
-
-  get() {
-    return AppStorage.get(this.KEY, {
-      chefId: null,
-      items: []
-    });
-  },
-
-  save(cart) {
-    AppStorage.set(this.KEY, cart);
-  },
-
-  clear() {
-    AppStorage.remove(this.KEY);
-  }
-};
