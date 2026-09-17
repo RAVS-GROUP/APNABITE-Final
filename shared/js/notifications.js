40f20de375b21716b400719e1c962bfbbe093605
RAVS <shubham110267@gmail.com>
2026-09-17 14:59:46 +0530

    Update notifications.js

--- shared/js/notifications.js
+++ shared/js/notifications.js
@@ -1,12 +1,1 @@
-const NotificationManager = {
 
-  unreadCount: 0,
-
-  setUnreadCount(count) {
-    this.unreadCount = Number(count || 0);
-  },
-
-  getUnreadCount() {
-    return this.unreadCount;
-  }
-};
