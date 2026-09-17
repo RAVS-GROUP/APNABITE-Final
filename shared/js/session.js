c941d787ba908dc73f18f18d391ff1688fc584ba
RAVS <shubham110267@gmail.com>
2026-09-17 15:04:33 +0530

    Update session.js

--- shared/js/session.js
+++ shared/js/session.js
@@ -1,1 +1,20 @@
+const SessionManager = {
 
+  KEY: "apnabite_session",
+
+  set(session) {
+    AppStorage.set(this.KEY, session);
+  },
+
+  get() {
+    return AppStorage.get(this.KEY, null);
+  },
+
+  clear() {
+    AppStorage.remove(this.KEY);
+  },
+
+  isLoggedIn() {
+    return !!this.get();
+  }
+};
