cf8d603645c3833bc9c2572fa6fea31089653423
RAVS <shubham110267@gmail.com>
2026-09-17 12:50:29 +0530

    Create location.js

---
+++ shared/js/location.js
@@ -0,0 +1,39 @@
+const LocationManager = {
+
+  current: null,
+
+  async getCurrentPosition() {
+
+    if (!navigator.geolocation) {
+      throw new Error(
+        "Location is not supported on this device."
+      );
+    }
+
+    return new Promise((resolve, reject) => {
+
+      navigator.geolocation.getCurrentPosition(
+        position => {
+
+          this.current = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            accuracy: position.coords.accuracy
+          };
+
+          resolve(this.current);
+        },
+
+        error => {
+          reject(error);
+        },
+
+        {
+          enableHighAccuracy: false,
+          timeout: 8000,
+          maximumAge: 300000
+        }
+      );
+    });
+  }
+};
